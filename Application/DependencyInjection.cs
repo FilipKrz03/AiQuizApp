28a4fcb521e902c641de06a4d41a7881b6544df1
Filip Krzemień <filipos2003@gmail.com>
2024-02-01 18:29:03 +0100

    feat: clean architecture project setup

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(configuration =>
+                configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+
+            return services;
+        }
+    }
+}
