a4afe4ff497b510d725fae63707125cd52cb595a
Filip Krzemień <filipos2003@gmail.com>
2024-02-04 23:39:58 +0100

    feat: added base quizes creator as a hosted service

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Application.Services;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,8 @@ namespace Application
         {
             services.AddMediatR(configuration =>
                 configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+
+            services.AddHostedService<BaseQuizesCreator>();
 
             return services;
         }
--- Application/Services/BaseQuizesCreator.cs
+++ Application/Services/BaseQuizesCreator.cs
@@ -1,7 +1,11 @@
 ﻿using Application.Props;
 using Domain.Entities;
+using Infrastructure.DbContexts;
 using Infrastructure.Interfaces;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,21 +14,27 @@ using System.Threading.Tasks;
 
 namespace Application.Services
 {
-    public class BaseQuizesCreator
+    public class BaseQuizesCreator : BackgroundService
     {
-        private readonly IRepository<Quiz> _quizRepository;
+        private readonly ILogger<BaseQuizesCreator> _logger;
+        private readonly IServiceProvider _serviceProvider;
 
-        public BaseQuizesCreator(
-            IRepository<Quiz> quizRepository
+        public BaseQuizesCreator( 
+            ILogger<BaseQuizesCreator> logger , 
+            IServiceProvider serviceProvider
             )
         {
-            _quizRepository = quizRepository;            
+            _logger = logger;
+            _serviceProvider = serviceProvider;
         }
 
-        public async Task CreateIfNotExist()
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            var scope = _serviceProvider.CreateScope();
+            var quizRepository = scope.ServiceProvider.GetRequiredService<IRepository<Quiz>>();
+
             var allQuizesTechnologies =
-                await _quizRepository.Query()
+                await quizRepository.Query()
                 .Select(e => e.TechnologyName)
                 .Distinct()
                 .ToListAsync();
@@ -34,7 +44,7 @@ namespace Application.Services
                 .Where(t => !allQuizesTechnologies.Contains(t))
                 .ToList();
 
-             // Todo 
+            // Todo
         }
     }
 }
