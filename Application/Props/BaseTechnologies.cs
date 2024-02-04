6d019845d2bc29f0eddb0c78fd6afaefb148f2ca
Filip Krzemień <filipos2003@gmail.com>
2024-02-04 22:38:28 +0100

    feat: added base technologies class

---
+++ Application/Props/BaseTechnologies.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Props
+{
+    public static class BaseTechnologies
+    {
+        public static IEnumerable<string> Get()
+        {
+            yield return "C#";
+            yield return "Java";
+            yield return "JavaScript";
+            yield return "Python";
+            yield return "PHP";
+            yield return "TypeScript";
+            yield return "C++";
+            yield return "React";
+            yield return "Kotlin";
+            yield return "SQL";
+        }
+    }
+}
