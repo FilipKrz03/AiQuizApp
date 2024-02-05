ef1cae303ea3ad8d23b6f1cb5f3b5c2a6409d04c
Filip Krzemień <filipos2003@gmail.com>
2024-02-05 23:48:55 +0100

    feat: added response dto's

---
+++ Application/Dto/QuizDetailResponseDto.cs
@@ -0,0 +1,17 @@
+﻿using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dto
+{
+    public class QuizDetailResponseDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string TechnologyName { get; set; } = string.Empty;
+        public int AdvanceNumber { get; set; }
+        public IEnumerable<QuestionResponseDto> Questions { get; set; } = [];
+    }
+}
