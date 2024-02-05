c6f102512435577b09d71caf8e2ed1e534d562ad
Filip Krzemień <filipos2003@gmail.com>
2024-02-05 19:05:04 +0100

    feat: added ai response to quiz entity converter

---
+++ Application/Interfaces/IAiQuestionsToQuizConverter.cs
@@ -0,0 +1,21 @@
+﻿using Application.Dto;
+using Domain.Entities;
+using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IAiQuestionsToQuizConverter
+    {
+        Quiz Convert(
+            IEnumerable<QuestionAiResponseDto> response,
+            string quizTitle,
+            string technologyName,
+            AdvanceNumber advanceNumber
+            );
+    }
+}
---
+++ Application/Services/AiQuestionsToQuizConverter.cs
@@ -0,0 +1,86 @@
+﻿using Application.Dto;
+using Application.Interfaces;
+using Domain.Entities;
+using Domain.ValueObjects;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public class AiQuestionsToQuizConverter : IAiQuestionsToQuizConverter
+    {
+        private readonly ILogger<AiQuestionsToQuizConverter> _logger;
+
+        public AiQuestionsToQuizConverter(ILogger<AiQuestionsToQuizConverter> logger) => _logger = logger;
+
+        public Quiz Convert
+            (IEnumerable<QuestionAiResponseDto> response, string quizTitle, string technologyName, AdvanceNumber advanceNumber)
+        {
+            var quizId = Guid.NewGuid();
+
+            Quiz quiz = new(quizId, quizTitle, technologyName, advanceNumber)
+            {
+                Questions = GetQuestions(response, quizId).ToList()
+            };
+
+            return quiz;
+        }
+
+        private IEnumerable<Question> GetQuestions(IEnumerable<QuestionAiResponseDto> questions, Guid quizId)
+        {
+            foreach (var question in questions)
+            {
+                var answerLetter = AnswerLetter.Create(question.CharOfProperAnswer);
+
+                if (answerLetter != null)
+                {
+                    var answers = question.Answers;
+
+                    var questionId = Guid.NewGuid();
+
+                    yield return new Question(questionId, question.QuestionContent, answerLetter)
+                    {
+                        QuizId = quizId,
+                        Answers = GetAnswers(answers, questionId).ToList()
+                    };
+                }
+            }
+        }
+
+        private IEnumerable<Answer> GetAnswers(IEnumerable<string> answers, Guid questionId)
+        {
+            foreach (var answer in answers)
+            {
+                var answerSplit = answer.Split(".");
+
+                char? answerLetter = GetAnswerLetter(answerSplit[0]);
+
+                if (answerLetter != null)
+                {
+                    var answerLetterObject = AnswerLetter.Create((char)answerLetter);
+
+                    yield return new Answer(Guid.NewGuid(), answerSplit[1], answerLetterObject!)
+                    {
+                        QuestionId = questionId,
+                    };
+                }
+                else
+                {
+                    _logger.LogWarning("AiQuestionsConverter - Cannot recognize answer letter : {answer}", answer);
+                }
+            }
+        }
+
+        private char? GetAnswerLetter(string answerString) => answerString switch
+        {
+            _ when answerString.Contains('a') => 'a',
+            _ when answerString.Contains('b') => 'b',
+            _ when answerString.Contains('c') => 'c',
+            _ when answerString.Contains('d') => 'd',
+            _ => null
+        };
+    }
+}
