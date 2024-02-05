f6589a818890398f8e1a40a97879d8f526b891bc
Filip Krzemień <filipos2003@gmail.com>
2024-02-05 16:41:36 +0100

    feat: first logic of quizes creator

---
+++ Application/Interfaces/IQuizesCreator.cs
@@ -0,0 +1,14 @@
+﻿using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+    public interface IQuizesCreator
+    {
+        Task Create(string technologyName , AdvanceNumber advanceNumber);
+    }
+}
--- Application/Services/QuizesCreator.cs
+++ Application/Services/QuizesCreator.cs
@@ -1,19 +1,36 @@
 ﻿using Domain.ValueObjects;
+using OpenAI.Managers;
+using OpenAI.ObjectModels;
+using OpenAI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OpenAI.ObjectModels.RequestModels;
+using Application.Props;
+using Application.Interfaces;
 
 namespace Application.Services
 {
-    public class QuizesCreator
+    public class QuizesCreator : IQuizesCreator
     {
-
-        public async Task Create
-            (string technologyName , AdvanceNumber advanceNumber)
+        public async Task Create(string technologyName, AdvanceNumber advanceNumber)
         {
+            var openAiService = new OpenAIService(new OpenAiOptions()
+            {
+                ApiKey = Environment.GetEnvironmentVariable("OpenAiApiKey")!,
+                DefaultModelId = Models.Gpt_3_5_Turbo
+            });
 
+            var completionResult =
+                await openAiService.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest()
+                {
+                    Messages = new List<ChatMessage>
+                    {
+                        ChatMessage.FromSystem(GptPrompts.CreateQuizPrompt(technologyName , advanceNumber.Number))
+                    }
+                });
         }
     }
 }
