78fe5169ace7e2007acbe08eaeb4465496ca996f
Filip Krzemień <filipos2003@gmail.com>
2024-02-06 00:13:34 +0100

    feat: added custom resource not found exception

---
+++ Domain/Exceptions/ResourceNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions
+{
+    public class ResourceNotFoundException : Exception
+    {
+        public ResourceNotFoundException(Guid id, string objectName = "Object")
+        : base($"{objectName} with id {id} not found") { }
+    }
+}
