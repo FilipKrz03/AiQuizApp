213e2107a797e29e388b900594247e220820f37d
Filip Krzemień <filipos2003@gmail.com>
2024-02-06 00:32:33 +0100

    feat: added quiz controller with get quiz details endopint

---
+++ Web/Controllers/QuizesController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Cqrs.Quiz.Query.GetQuiz;
+using Application.Dto;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/quizes")]
+    [ApiController]
+    public class QuizesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public QuizesController(IMediator mediator) => _mediator = mediator;
+
+        [HttpGet("{quizId}")]
+        public async Task<QuizDetailResponseDto> GetQuizDetails(Guid quizId)
+        {
+            var result = await _mediator.Send(new GetQuizQuery(quizId));
+
+            return result;
+        }
+    }
+}
