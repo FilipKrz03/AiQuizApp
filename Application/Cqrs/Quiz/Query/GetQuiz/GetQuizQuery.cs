ca0dee1d44c0a01478aed469891caf28f856f8ef
Filip Krzemień <filipos2003@gmail.com>
2024-02-06 00:32:04 +0100

    feat: added get quiz query (handler)

---
+++ Application/Cqrs/Quiz/Query/GetQuiz/GetQuizQuery.cs
@@ -0,0 +1,20 @@
+﻿using Application.Dto;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Cqrs.Quiz.Query.GetQuiz
+{
+    public class GetQuizQuery : IRequest<QuizDetailResponseDto>
+    {
+        public Guid Id { get; init; }
+
+        public GetQuizQuery(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ Application/Cqrs/Quiz/Query/GetQuiz/GetQuizQueryHandler.cs
@@ -0,0 +1,43 @@
+﻿using Application.Dto;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Exceptions;
+using Infrastructure.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Cqrs.Quiz.Query.GetQuiz
+{
+    public class GetQuizQueryHandler : IRequestHandler<GetQuizQuery, QuizDetailResponseDto>
+    {
+        private readonly IRepository<Domain.Entities.Quiz> _quizRepository;
+        private readonly IMapper _mapper;
+
+        public GetQuizQueryHandler(
+            IRepository<Domain.Entities.Quiz> quizRepository,
+            IMapper mapper
+            )
+        {
+            _quizRepository = quizRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<QuizDetailResponseDto> Handle(GetQuizQuery request, CancellationToken cancellationToken)
+        {
+            var quiz = await _quizRepository
+                .GetByIdQuery(request.Id)
+                .Include(q => q.Questions)
+                .ThenInclude(q => q.Answers)
+                .FirstOrDefaultAsync();
+
+            if (quiz == null) throw new ResourceNotFoundException(request.Id, "Quiz");
+           
+            return _mapper.Map<QuizDetailResponseDto>(quiz)!;
+        }
+    }
+}
