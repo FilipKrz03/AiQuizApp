b0c5d030b999be57ddbad5c4f6caea2a22c388af
Filip Krzemień <filipos2003@gmail.com>
2024-02-07 03:55:08 +0100

    feat: added appropriate mapping and convering to paged list

---
+++ Application/Profiles/Converters/PagedListConverter.cs
@@ -0,0 +1,24 @@
+﻿using Application.Common;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Profiles.Converters
+{
+    public class PagedListConverter<TSource, TDestination> :
+        ITypeConverter<PagedList<TSource>, PagedList<TDestination>>
+    {
+        public PagedList<TDestination> 
+            Convert(PagedList<TSource> source, PagedList<TDestination> destination, ResolutionContext context)
+        {
+            var mappedCollecton =
+                context.Mapper.Map<List<TSource>, List<TDestination>>(source);
+
+            return new PagedList<TDestination>
+                (mappedCollecton!, source.PageSize, source.PageNumber, source.TotalCount);
+        }
+    }
+}
---
+++ Application/Profiles/PagedListProfile.cs
@@ -0,0 +1,20 @@
+﻿using Application.Common;
+using Application.Profiles.Converters;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Profiles
+{
+    public class PagedListProfile : Profile
+    {
+        public PagedListProfile()
+        {
+            CreateMap(typeof(PagedList<>), typeof(PagedList<>))
+                .ConvertUsing(typeof(PagedListConverter<,>));
+        }
+    }
+}
