431035e9c9eef2037aef212d92d810e2c0c15a15
Filip Krzemień <filipos2003@gmail.com>
2024-02-07 03:11:41 +0100

    feat: added resource paramethers

---
+++ Application/Common/ResourceParamethers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common
+{
+    public class ResourceParamethers
+    {
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+        public string? SortColumn { get; set; }
+        public string SortOrder { get; set; } = "asc";
+    }
+}
