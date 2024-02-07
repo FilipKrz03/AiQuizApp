e06726fddeeddc1d56365afa798587d09a184bc3
Filip Krzemień <filipos2003@gmail.com>
2024-02-07 15:55:11 +0100

    test: common tests added

---
+++ UnitTests/Application/Common/PagedListTests.cs
@@ -0,0 +1,73 @@
+﻿using Application.Common;
+using Domain.Entities;
+using Domain.ValueObjects;
+using FluentAssertions;
+using MockQueryable.Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Application.Common
+{
+    public class PagedListTests
+    {
+        [Theory]
+        [InlineData(10, 25, 3)]
+        [InlineData(15, 30, 2)]
+        [InlineData(15, 31, 3)]
+        public void PagedList_Should_ProperlyCalculateTotalPagesCount(int pageSize, int totalCount, int expectedTotalPages)
+        {
+            List<string> items = [];
+
+            PagedList<string> pagedList = new(items, pageSize, 1, totalCount);
+
+            pagedList.TotalPages
+                .Should()
+                .Be(expectedTotalPages);
+        }
+
+        [Fact]
+        public async Task CreateAsync_Should_ProperlyCreatePagedList()
+        {
+            List<Quiz> items = new()
+            {
+                new Quiz(Guid.NewGuid() , "Quiz1", "" , AdvanceNumber.Create(5)!) ,
+                new Quiz(Guid.NewGuid() , "Quiz2", "" , AdvanceNumber.Create(5)!) ,
+                new Quiz(Guid.NewGuid() , "Quiz3", "" , AdvanceNumber.Create(5)!) ,
+                new Quiz(Guid.NewGuid() , "Quiz4", "" , AdvanceNumber.Create(5)!)
+            };
+
+            var result = await PagedList<Quiz>.CreateAsync(items.BuildMock(), 2, 2);
+
+            result[0].Title
+                .Should()
+                .Be("Quiz3");
+
+            result[1].Title
+                .Should()
+                .Be("Quiz4");
+
+            result.TotalCount
+                .Should()
+                .Be(4);
+
+            result.PageNumber
+                .Should()
+                .Be(2);
+
+            result.HasNext
+                .Should()
+                .Be(false);
+
+            result.HasPrevious
+                .Should()
+                .Be(true);
+
+            result.TotalPages
+                .Should()
+                .Be(2);
+        }
+    }
+}
---
+++ UnitTests/Application/Common/ResourceParamethersTests.cs
@@ -0,0 +1,33 @@
+﻿using Application.Common;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Application.Common
+{
+    public class ResourceParamethersTests
+    {
+        [Fact]
+        public void PageSize_ShouldBeNotGraeterThan30EvenIfUserProvidedGreaterNumber()
+        {
+            ResourceParamethers resourceParamethers = new() { PageSize = 100 };
+
+            resourceParamethers.PageSize
+                .Should()
+                .Be(30);
+        }
+
+        [Fact]
+        public void SortOrder_ShouldHaveDefaultAscValue()
+        {
+            ResourceParamethers resourceParamethers = new();
+
+            resourceParamethers.SortOrder
+                .Should()
+                .Be("asc");
+        }
+    }
+}
