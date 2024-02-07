042eeb0b04b332add91a0513765035e0544dbe8e
Filip Krzemień <filipos2003@gmail.com>
2024-02-07 03:27:06 +0100

    feat : added pagination logic

---
+++ Application/Common/PagedList.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common
+{
+    public class PagedList<T> : List<T>
+    {
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
+
+        public int TotalCount { get; set; }
+        public int TotalPages { get; set; }
+
+        public bool HasPrevious => PageNumber > 1;
+        public bool HasNext => PageNumber < TotalPages;
+
+        private PagedList(List<T> items, int pageSize, int pageNumber, int totalCount)
+        {
+            PageSize = pageSize;
+            PageNumber = pageNumber;
+            TotalCount = totalCount;
+            TotalPages = (int)Math.Ceiling(TotalCount / (double)PageSize);
+            AddRange(items);
+        }
+
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int pageSize, int pageNumber)
+        {
+            int totalCount = await source.CountAsync();
+
+            var items = await source
+                .Skip(pageSize * (pageNumber - 1))
+                .Take(pageSize)
+                .ToListAsync();
+
+            return new(items, pageSize, pageNumber, totalCount);
+        }
+    }
+}
--- Application/Common/ResourceParamethers.cs
+++ Application/Common/ResourceParamethers.cs
@@ -8,7 +8,16 @@ namespace Application.Common
 {
     public class ResourceParamethers
     {
-        public int PageSize { get; set; }
+        private const int maxPageSize = 30;
+        private int pageSize = 20;
+        public int PageSize
+        {
+            get { return pageSize; }
+            set
+            {
+                pageSize = value > maxPageSize ? maxPageSize : value;
+            }
+        }
         public int PageNumber { get; set; }
         public string? SortColumn { get; set; }
         public string SortOrder { get; set; } = "asc";
