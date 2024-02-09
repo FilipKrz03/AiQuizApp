256fc7b00a682caeaabd6df5a5f28c65a997eeb5
Filip Krzemień <filipos2003@gmail.com>
2024-02-09 15:54:06 +0100

    feat: added quizes page that fetch quiz data from api

---
+++ Client/Services/IQuizDataService.cs
@@ -0,0 +1,9 @@
+﻿using Application.Dto;
+
+namespace Client.Services
+{
+	public interface IQuizDataService
+	{
+		Task<IEnumerable<QuizBasicResponseDto>> GetQuizes();
+	}
+}
---
+++ Client/Services/QuizDataService.cs
@@ -0,0 +1,21 @@
+﻿using Client.Pages;
+using Newtonsoft.Json;
+using Application.Dto;
+
+namespace Client.Services
+{
+	public class QuizDataService(HttpClient httpClient) : IQuizDataService
+	{
+		private readonly HttpClient _httpClient = httpClient;
+
+		public async Task <IEnumerable<QuizBasicResponseDto>> GetQuizes()
+		{
+			var result = await _httpClient.GetAsync("api/quizes");
+
+			var data = JsonConvert.DeserializeObject<IEnumerable<QuizBasicResponseDto>>
+				(await result.Content.ReadAsStringAsync());
+
+			return data!;
+		}
+	}
+}
