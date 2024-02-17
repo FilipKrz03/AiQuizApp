77832b984c9ff64cb9fff4b9ca085ea02a9f8bec
Filip Krzemień <filipos2003@gmail.com>
2024-02-17 18:50:54 +0100

    feat: added login page

---
+++ Client/Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Client.Models
+{
+	public class LoginModel
+	{
+		[Required(ErrorMessage = "Email nie moze byc pusty"), EmailAddress(ErrorMessage = "Pole email powinno byc poprawnym mailem")]
+		public string Email { get; set; } = string.Empty;
+
+		[
+			Required(ErrorMessage = "Haslo nie moze byc puste"),
+			RegularExpression
+			(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{6,}$",
+			ErrorMessage = "Hasło musi miec co najmniej: 6 znakow , jedna duza litere , jedna małą litere , i cyfre"
+			)]
+		public string Password { get; set; } = string.Empty;
+	}
+}
