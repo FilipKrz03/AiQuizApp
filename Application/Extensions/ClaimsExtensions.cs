111b16124a6ef0c4b017c59cb554188f2c5d5c68
Filip Krzemień <filipos2003@gmail.com>
2024-02-22 03:51:44 +0100

    feat: added claims extensions class

---
+++ Application/Extensions/ClaimsExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Extensions
+{
+	public static class ClaimsExtensions
+	{
+		public static string? GetId(this IEnumerable<Claim> claims) => 
+			claims.Where(e => e.Type == ClaimTypes.NameIdentifier).FirstOrDefault()?.Value;
+	}
+}
