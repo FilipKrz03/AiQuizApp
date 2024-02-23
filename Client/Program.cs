dc5aea24f0fb3a347bed589f109d0eda9dc7c223
Filip Krzemień <filipos2003@gmail.com>
2024-02-23 18:46:37 +0100

    feat: registered new services

--- Client/Program.cs
+++ Client/Program.cs
@@ -10,13 +10,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-//builder.Services.AddHttpClient("api", cfg =>
-//{
-//	cfg.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
-//})
-//	.AddHttpMessageHandler<AccessTokenRequestHandler>();
-
 builder.Services.AddScoped<AccessTokenRequestHandler>();
 builder.Services.AddScoped<IDataService, DataService>();
 builder.Services.AddScoped<CustomAuthStateProvider>();
