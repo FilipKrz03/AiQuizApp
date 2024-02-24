caec9956505efe1859cf190bf7c54f83ce4aa81b
Filip Krzemień <filipos2003@gmail.com>
2024-02-24 12:56:08 +0100

    feat: added user quiz controller

---
+++ Web/Controllers/UserQuizesController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Cqrs.UserQuiz.Command.CreateQuizCommand;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+	[Route("api/user/quizes")]
+	[Authorize]
+	[ApiController]
+	public class UserQuizesController(IMediator mediator) : ControllerBase
+	{
+		private readonly IMediator _mediator = mediator;
+
+		[HttpPost]
+		public async Task<ActionResult> CreateQuiz([FromBody] CreateAiQuizCommand command)
+		{
+			var result = await _mediator.Send(command);
+
+			return Ok(result);
+		}
+	}
+}
