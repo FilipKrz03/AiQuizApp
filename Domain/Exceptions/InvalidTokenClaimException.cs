25321084e520ae4e3a3f8fa0ad7d0eea7ff674a0
Filip Krzemień <filipos2003@gmail.com>
2024-03-02 23:05:16 +0100

    feat: added invalid token claim exception

---
+++ Domain/Exceptions/InvalidTokenClaimException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions
+{
+	public class InvalidTokenClaimException : Exception
+	{
+		public InvalidTokenClaimException()
+		   : base("User with id from your token do not exist") { }
+	}
+}
