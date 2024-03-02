40f0a0c5359b8fc60633b44b9f010e1fd06b7026
Filip Krzemień <filipos2003@gmail.com>
2024-03-02 23:58:59 +0100

    refactor: refactored  models dependencies

--- Infrastructure/DbContexts/QuizApplicationDbContext.cs
+++ Infrastructure/DbContexts/QuizApplicationDbContext.cs
@@ -10,31 +10,41 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Infrastructure.DbContexts
 {
-    public class QuizApplicationDbContext : IdentityDbContext<User>
-    {
-        public DbSet<Quiz> Quizzes { get; set; }
-        public DbSet<Question> Questions { get; set; }
-        public DbSet<Answer> Answers { get; set; }
-        public DbSet<UserOwnQuiz> UserOwnQuizzes { get; set; }
+	public class QuizApplicationDbContext : IdentityDbContext<User>
+	{
+		public DbSet<Quiz> Quizzes { get; set; }
+		public DbSet<Question> Questions { get; set; }
+		public DbSet<Answer> Answers { get; set; }
+		public DbSet<UserOwnQuiz> UserOwnQuizzes { get; set; }
 
-        public QuizApplicationDbContext(DbContextOptions<QuizApplicationDbContext> options)
-            : base(options) { }
+		public QuizApplicationDbContext(DbContextOptions<QuizApplicationDbContext> options)
+			: base(options) { }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Question>()
-                .Property(p => p.ProperAnswerLetter)
-                .HasConversion(
-                ProperAnswerLetter => ProperAnswerLetter.Letter,
-                value => AnswerLetter.Create(value)!
-                );
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Question>()
+				.Property(p => p.ProperAnswerLetter)
+				.HasConversion(
+				ProperAnswerLetter => ProperAnswerLetter.Letter,
+				value => AnswerLetter.Create(value)!
+				);
 
-            modelBuilder.Entity<Quiz>()
-                .Property(p => p.AdvanceNumber)
-                .HasConversion(
-                 AdvanceNumber => AdvanceNumber.Number,
-                 value => AdvanceNumber.Create(value)!
-                 );
+			modelBuilder.Entity<Quiz>()
+				.Property(p => p.AdvanceNumber)
+				.HasConversion(
+				 AdvanceNumber => AdvanceNumber.Number,
+				 value => AdvanceNumber.Create(value)!
+				 );
+
+			modelBuilder.Entity<Quiz>()
+			.HasMany(x => x.Questions)
+			.WithOne(x => x.Quiz)
+			.OnDelete(DeleteBehavior.Cascade);
+
+			modelBuilder.Entity<Question>()
+				.HasOne(x => x.Quiz)
+				.WithMany(x => x.Questions)
+				.OnDelete(DeleteBehavior.Cascade);
 
 			modelBuilder.Entity<UserOwnQuiz>()
 			  .Property(p => p.AdvanceNumber)
@@ -44,16 +54,16 @@ namespace Infrastructure.DbContexts
 			   );
 
 			modelBuilder.Entity<Answer>()
-                .Property(p => p.AnswerLetter)
-                .HasConversion(
-                 AnswerLetter => AnswerLetter.Letter,
-                 value => AnswerLetter.Create(value)!
-                );
+				.Property(p => p.AnswerLetter)
+				.HasConversion(
+				 AnswerLetter => AnswerLetter.Letter,
+				 value => AnswerLetter.Create(value)!
+				);
 
-            modelBuilder.Entity<Quiz>().ToTable("Quizzes");
-            modelBuilder.Entity<UserOwnQuiz>().ToTable("UserOwnQuizzes");
+			modelBuilder.Entity<Quiz>().ToTable("Quizzes");
+			modelBuilder.Entity<UserOwnQuiz>().ToTable("UserOwnQuizzes");
 
-            base.OnModelCreating(modelBuilder);
-        }
-    }
+			base.OnModelCreating(modelBuilder);
+		}
+	}
 }
---
+++ Infrastructure/Migrations/20240302225703_UptadedModelDependencies.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UptadedModelDependencies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_Quizzes_QuizId",
+                table: "Questions");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_Quizzes_QuizId",
+                table: "Questions",
+                column: "QuizId",
+                principalTable: "Quizzes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Questions_Quizzes_QuizId",
+                table: "Questions");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Questions_Quizzes_QuizId",
+                table: "Questions",
+                column: "QuizId",
+                principalTable: "Quizzes",
+                principalColumn: "Id");
+        }
+    }
+}
