96a4440aa89789e5bc9d2c82971b957a4d18a875
Filip Krzemień <filipos2003@gmail.com>
2024-03-02 23:42:58 +0100

    feat: added ResourceAlreadyNotExistException

---
+++ Domain/Exceptions/ResourceAlreadyNotExistException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Exceptions
+{
+	public class ResourceAlreadyNotExistException : Exception
+	{
+		public ResourceAlreadyNotExistException(Guid id, string objectName = "Object")
+			: base($"{objectName} with id {id} already do not exist") { }
+	}
+}
--- Web/Middleware/ExceptionHandlingMiddleware.cs
+++ Web/Middleware/ExceptionHandlingMiddleware.cs
@@ -3,40 +3,45 @@ using Domain.Exceptions;
 
 namespace Web.Middleware
 {
-    public sealed class ExceptionHandlingMiddleware(
-        ILogger<ExceptionHandlingMiddleware> logger
-        ) : IMiddleware
-    {
-        private readonly ILogger<ExceptionHandlingMiddleware> _logger = logger;
+	public sealed class ExceptionHandlingMiddleware(
+		ILogger<ExceptionHandlingMiddleware> logger
+		) : IMiddleware
+	{
+		private readonly ILogger<ExceptionHandlingMiddleware> _logger = logger;
 
-        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
-        {
-            try
-            {
-                await next.Invoke(context);
-            }
-            catch(ResourceNotFoundException ex)
-            {
-                context.Response.StatusCode = StatusCodes.Status404NotFound;
-                await context.Response.WriteAsync(ex.Message);
-            }
-            catch(InvalidAccesTokenException ex)
-            {
-                context.Response.StatusCode = StatusCodes.Status400BadRequest;
-                await context.Response.WriteAsync(ex.Message);
-            }
-            catch(InvalidTokenClaimException ex)
-            {
+		public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+		{
+			try
+			{
+				await next.Invoke(context);
+			}
+			catch (ResourceNotFoundException ex)
+			{
+				context.Response.StatusCode = StatusCodes.Status404NotFound;
+				await context.Response.WriteAsync(ex.Message);
+			}
+			catch (InvalidAccesTokenException ex)
+			{
 				context.Response.StatusCode = StatusCodes.Status400BadRequest;
 				await context.Response.WriteAsync(ex.Message);
 			}
-            catch (Exception ex)
-            {
-                _logger.LogError("ExceptionHandlingMiddleware - {ex}", ex.Message);
+			catch (InvalidTokenClaimException ex)
+			{
+				context.Response.StatusCode = StatusCodes.Status400BadRequest;
+				await context.Response.WriteAsync(ex.Message);
+			}
+			catch(ResourceAlreadyNotExistException ex)
+			{
+				context.Response.StatusCode = StatusCodes.Status404NotFound;
+				await context.Response.WriteAsync(ex.Message);
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError("ExceptionHandlingMiddleware - {ex}", ex.Message);
 
-                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                await context.Response.WriteAsync("Error occured on server side");
-            }
-        }
-    }
+				context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+				await context.Response.WriteAsync("Error occured on server side");
+			}
+		}
+	}
 }
