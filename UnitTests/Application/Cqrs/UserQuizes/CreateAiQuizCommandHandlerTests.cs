c4746c2db9ed68ed8b7b15a6ce2dd9f9c31e9488
Filip Krzemień <filipos2003@gmail.com>
2024-03-11 01:25:32 +0100

    test: added create ai quiz command handler tests

---
+++ UnitTests/Application/Cqrs/UserQuizes/CreateAiQuizCommandHandlerTests.cs
@@ -0,0 +1,56 @@
+﻿using Application.Cqrs.UserQuiz.Command.CreateAiQuiz;
+using Domain.Exceptions;
+using FluentAssertions;
+using Infrastructure.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Application.Cqrs.UserQuizes
+{
+	public class CreateAiQuizCommandHandlerTests
+	{
+		private readonly Mock<IServiceProvider> _serviceProviderMock;
+		private readonly Mock<IUserRepository> _userRepositoryMock;
+		private readonly CreateAiQuizCommandHandler _handler;
+
+        public CreateAiQuizCommandHandlerTests()
+        {
+			_serviceProviderMock = new();
+			_userRepositoryMock = new();
+
+			_handler = new(_serviceProviderMock.Object, _userRepositoryMock.Object);
+        }
+
+		[Fact]
+		public async Task Handler_Should_ThrowInvalidTokenClaimException_WhenUserWithIdFromTokenClaimNotFound()
+		{
+			_userRepositoryMock.Setup(x => x.UserExistAsync(It.IsAny<string>()))
+			   .ReturnsAsync(false);
+
+			await _handler.Invoking(x => x.Handle(new CreateAiQuizCommand("" , "" , 5 , ""), default!))
+				.Should()
+				.ThrowAsync<InvalidTokenClaimException>();
+		}
+
+		[Fact]	
+		public async Task Handler_Should_ReturnProperStringInformationWhenUserExist()
+		{
+			_userRepositoryMock.Setup(x => x.UserExistAsync(It.IsAny<string>()))
+			   .ReturnsAsync(true);
+
+			var result = await _handler.Handle(new CreateAiQuizCommand("", "", 5, ""), default!);
+
+			result
+				.Should()
+				.NotBeNull();
+
+			result
+				.Should()
+				.BeOfType<string>();
+		}	
+    }
+}
