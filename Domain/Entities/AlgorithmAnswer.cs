2da8bf738dc0c18fb0fff7115c53d6af2fab962d
Filip Krzemień <filipos2003@gmail.com>
2024-03-14 03:00:49 +0100

    feat: added algorithm task and algorithm answert models

---
+++ Domain/Entities/AlgorithmAnswer.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+	public class AlgorithmAnswer : Entity
+	{
+		public string AnswerContent { get; set; }
+		public string Language { get; set; }
+
+		[ForeignKey("AlgorithmTaskId")]
+		public AlgorithmTask AlgorithmTask { get; set; } = default!;
+		public Guid AlgorithmTaskId { get; set; }
+
+		public AlgorithmAnswer(Guid id, string answerContent, string language)
+			: base(id)
+		{
+			Language = language;
+			AnswerContent = answerContent;
+		}
+	}
+}
---
+++ Domain/Entities/AlgorithmTask.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Common;
+using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+	public class AlgorithmTask : Entity
+	{
+		public string TaskTitle { get; set; }
+		public string TaskContent { get; set; }
+		public AdvanceNumber AdvanceNumber { get; set; }
+		public List<AlgorithmAnswer> Answers { get; set; } = [];
+
+		public AlgorithmTask(Guid id, string taskTitle, string taskContent, AdvanceNumber advanceNumber)
+			: base(id)
+		{
+			TaskTitle = taskTitle;
+			TaskContent = taskContent;
+			AdvanceNumber = advanceNumber;
+		}
+	}
+}
