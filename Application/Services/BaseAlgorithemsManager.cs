8a47f5887dcc890bb7380f5ad1608996303afc96
Filip Krzemień <filipos2003@gmail.com>
2024-03-14 03:49:15 +0100

    feat: added base algorithems manager

---
+++ Application/Services/BaseAlgorithemsManager.cs
@@ -0,0 +1,70 @@
+﻿using Application.Interfaces;
+using Application.Props;
+using Domain.Entities;
+using Domain.ValueObjects;
+using Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+	public class BaseAlgorithemsManager(
+		ILogger<BaseAlgorithemsManager> logger,
+		IServiceProvider serviceProvider
+		) : BackgroundService
+	{
+		private readonly ILogger<BaseAlgorithemsManager> _logger = logger;
+		private readonly IServiceProvider _serviceProvider = serviceProvider;
+
+		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+		{
+			using IServiceScope scope = _serviceProvider.CreateScope();
+			{
+				var algorithemsCreator = scope.ServiceProvider.GetRequiredService<IAlgorithemsCreator>();
+				var algoithmTasksRepository = scope.ServiceProvider.GetRequiredService<IRepository<AlgorithmTask>>();
+
+				var allAlgorithmsTopics =
+					   await algoithmTasksRepository
+					   .Query()
+					   .Select(e => e.TaskMainTopics)
+					   .Distinct()
+					   .ToListAsync();
+
+
+				var baseTopicsWithNoAlgoritmsTasks =
+					BaseAlgorithmsTasksTopics.Get()
+					.Where(t => !allAlgorithmsTopics.Contains(t))
+					.ToList();
+
+				foreach (var topic in baseTopicsWithNoAlgoritmsTasks)
+				{
+					var advanceNumber = DrawAdvanceNumber();
+
+					var algorithm = await algorithemsCreator.CreateAsync(advanceNumber, $"Task {advanceNumber.Number} / 10", topic);
+
+					if (algorithm != null)
+					{
+						algoithmTasksRepository.Insert(algorithm);
+						await algoithmTasksRepository.SaveChangesAsync();
+
+						_logger.LogInformation("BaseAlgorithmsManager - new base algorithm added to db");
+					}
+				}
+			}
+		}
+
+		private AdvanceNumber DrawAdvanceNumber()
+		{
+			Random random = new();
+
+			return AdvanceNumber.Create(random.Next(1, 10))!;
+		}
+	}
+}
