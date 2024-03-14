4a1750c474f2e5fc3792e2992191235927c1e37c
Filip Krzemień <filipos2003@gmail.com>
2024-03-14 03:48:30 +0100

    feat: added algorithems creator

---
+++ Application/Interfaces/IAlgorithemsCreator.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Interfaces
+{
+	public interface IAlgorithemsCreator
+	{
+		Task<AlgorithmTask?> CreateAsync(AdvanceNumber advanceNumber, string taskTitle, string specialTopics);
+	}
+}
---
+++ Application/Services/AlgorithemsCreator.cs
@@ -0,0 +1,67 @@
+﻿using Application.Props;
+using Domain.ValueObjects;
+using OpenAI.Managers;
+using OpenAI.ObjectModels.RequestModels;
+using OpenAI.ObjectModels;
+using OpenAI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Application.Dto;
+using Application.Interfaces;
+using Domain.Entities;
+
+namespace Application.Services
+{
+	public class AlgorithemsCreator(
+		ILogger<AlgorithemsCreator> logger , 
+		IAiAlgorithmsConverter aiAlgorithmsConverter
+		) : IAlgorithemsCreator
+	{
+		private readonly ILogger<AlgorithemsCreator> _logger = logger;
+		private readonly IAiAlgorithmsConverter _aiAlgorithmsConverter = aiAlgorithmsConverter;
+
+		public async Task<AlgorithmTask?> CreateAsync(AdvanceNumber advanceNumber, string taskTitle , string specialTopics)
+		{
+			var openAiService = new OpenAIService(new OpenAiOptions()
+			{
+				ApiKey = Environment.GetEnvironmentVariable("OpenAiApiKey")!,
+				DefaultModelId = Models.Gpt_3_5_Turbo
+			});
+
+			var completionResult =
+				await openAiService.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest()
+				{
+					Messages = new List<ChatMessage>
+					{
+						ChatMessage.FromSystem(GptPrompts.CreateAlgorithmPrompt(advanceNumber.Number , specialTopics))
+					}
+				});
+
+			var body = completionResult.Choices.First().Message.Content;
+
+			if (body == null)
+			{
+				_logger.LogWarning("Algorithems creator - body of Ai response is null !");
+				return null;
+			}
+
+			try
+			{
+				var algorithm = JsonConvert.DeserializeObject<AlgorithmAiResponseDto>(body);
+
+				return _aiAlgorithmsConverter.ConvertToAlgorithmTask(algorithm! , taskTitle, advanceNumber , specialTopics);
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError("ex - {ex}", ex);
+
+				return null;
+			}
+		}
+	}
+}
