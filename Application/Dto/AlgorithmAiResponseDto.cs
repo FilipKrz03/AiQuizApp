f5850f28686e2493297db2ae370d372d61d584d1
Filip Krzemień <filipos2003@gmail.com>
2024-03-14 03:05:51 +0100

    feat: added ai algorithm responses models

---
+++ Application/Dto/AlgorithmAiResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dto
+{
+	public class AlgorithmAiResponseDto
+	{
+		public string QuestionContent { get; set; } = string.Empty;
+		public IEnumerable<ProgrammingLanguageWithAnswerAiResponseDto> LanguageAiResponseDtos { get; set; } = [];
+	}
+}
---
+++ Application/Dto/ProgrammingLanguageWithAnswerAiResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dto
+{
+	public class ProgrammingLanguageWithAnswerAiResponseDto
+	{
+		public string LanguageName { get; set; } = string.Empty;
+		public string Content { get; set; } = string.Empty;
+	}
+}
