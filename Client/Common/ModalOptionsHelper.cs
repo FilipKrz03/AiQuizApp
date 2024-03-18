5b8785839c0bb4e4462c211fcc4e2fb3ea0976d2
Filip Krzemień <filipos2003@gmail.com>
2024-03-18 13:20:39 +0100

    feat: improved modal looking

---
+++ Client/Common/ModalOptionsHelper.cs
@@ -0,0 +1,17 @@
+﻿using Blazored.Modal;
+
+namespace Client.Common
+{
+	public static class ModalOptionsHelper
+	{
+		public static ModalOptions GetForResultPopup()
+		{
+			return new ModalOptions()
+			{
+				HideHeader = true,
+				Position = ModalPosition.BottomRight,
+				AnimationType = ModalAnimationType.PopIn
+			};
+		}
+	}
+}
