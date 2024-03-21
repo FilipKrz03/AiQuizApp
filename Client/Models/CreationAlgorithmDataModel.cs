b39a66a488b2dd679e1c5f4b66bc94116f638411
Filip Krzemień <filipos2003@gmail.com>
2024-03-22 00:06:20 +0100

    feat: added quiz and algorithm creation validation in forms

--- Client/Models/CreationAlgorithmDataModel.cs
+++ Client/Models/CreationAlgorithmDataModel.cs
@@ -1,9 +1,17 @@
-﻿namespace Client.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Client.Models
 {
 	public class CreationAlgorithmDataModel 
 	{
+		[Required(ErrorMessage = "Musisz podac tytuł zdania")]
 		public string TaskTitle { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = "Musisz podać główne zagadnienia zadania")]
 		public string TaskMainTopics { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = "Poziom zaawansowania jest wymagany")
+			, Range(1 , 10 , ErrorMessage = "Poziom zaawansowania powinien być miedzy 1 a 10")]
 		public int AdvanceNumber { get; set; } = 4;
 	}
 }
--- Client/Models/CreationQuizDataModel.cs
+++ Client/Models/CreationQuizDataModel.cs
@@ -4,11 +4,14 @@ namespace Client.Models
 {
 	public class CreationQuizDataModel
 	{
-		[Required]
+		[Required (ErrorMessage = "Tytuł quizu jest wymagany")]
 		public string QuizTitle { get; set; } = string.Empty;
 
-		[Required]	
+		[Required(ErrorMessage = "Technologia quizu jest wymagana")]	
 		public string TechnologyName { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = "Poziom zaawansowania jest wymagany")
+		, Range(1, 10, ErrorMessage = "Poziom zaawansowania powinien być miedzy 1 a 10")]
 		public int AdvanceNumber { get; set; } = 4;
 	}
 }
