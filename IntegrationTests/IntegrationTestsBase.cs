8b51c8b636171be32b7c2a3701a3d8cfd09ba665
Filip Krzemień <filipos2003@gmail.com>
2024-03-24 16:54:55 +0100

    test: added integration tests setup

---
+++ IntegrationTests/IntegrationTestsBase.cs
@@ -0,0 +1,47 @@
+﻿using Infrastructure.DbContexts;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace IntegrationTests
+{
+	public class IntegrationTestsBase
+	{
+		protected readonly WebApplicationFactory<Program> _factory;
+		protected readonly HttpClient _httpClient;
+
+		public IntegrationTestsBase()
+		{
+			var appFactory = new WebApplicationFactory<Program>()
+				.WithWebHostBuilder(builder =>
+				{
+					builder.ConfigureTestServices(services =>
+					{
+						services.RemoveAll(typeof(IHostedService));
+						services.RemoveAll(typeof(QuizApplicationDbContext));
+
+						services.AddDbContext<QuizApplicationDbContext>(options =>
+						{
+							options.UseInMemoryDatabase("TestDb");
+						});
+
+						var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<QuizApplicationDbContext>));
+
+						if (descriptor != null)
+						{
+							services.Remove(descriptor);
+						}
+
+						services.AddDbContext<QuizApplicationDbContext>(options =>
+						{
+							options.UseInMemoryDatabase("TestDb");
+						});
+					});
+				});
+
+			_factory = appFactory;
+			_httpClient = appFactory.CreateClient();
+		}
+	}
+}
