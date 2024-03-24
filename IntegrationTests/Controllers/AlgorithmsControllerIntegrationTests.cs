1054fed02740205b56754a0e6b6c19df9878c746
Filip Krzemień <filipos2003@gmail.com>
2024-03-24 17:54:11 +0100

    test: added algorithms controller integration tests

---
+++ IntegrationTests/Controllers/AlgorithmsControllerIntegrationTests.cs
@@ -0,0 +1,94 @@
+﻿using Application.Dto;
+using Domain.Entities;
+using Domain.ValueObjects;
+using FluentAssertions;
+using Newtonsoft.Json;
+
+namespace IntegrationTests.Controllers
+{
+	public class AlgorithmsControllerIntegrationTests : IntegrationTestsBase
+	{
+		[Fact]
+		public async Task Controller_GetAlgorithms_ShouldReturn200StatusCode()
+		{
+			var reqeust = await _httpClient.GetAsync("api/algorithms");
+
+			reqeust.StatusCode
+				.Should()
+				.Be(System.Net.HttpStatusCode.OK);
+		}
+
+		[Fact]
+		public async Task Controller_GetAlgorithms_Shold_ReturnProperlySortedAndSearchedAlgorithms()
+		{
+			List<AlgorithmTask> algoritms = [
+				new AlgorithmTask(Guid.NewGuid(), "BubbleSorting", "Test", "Test", AdvanceNumber.Create(5)!),
+				new AlgorithmTask(Guid.NewGuid(), "Numbers", "Test", "Test", AdvanceNumber.Create(5)!),
+				new AlgorithmTask(Guid.NewGuid(), "QuickSorting", "Test", "Test", AdvanceNumber.Create(5)!),
+				new AlgorithmTask(Guid.NewGuid(), "Binary", "Test", "Test", AdvanceNumber.Create(5)!)
+			];
+
+			DbSeeder(db => db.AddRange(algoritms));
+
+			var result = await _httpClient.GetAsync("api/algorithms?SearchQuery=Sorting&SortColumn=taskTitle&SortOrder=desc");
+
+			var content = JsonConvert.DeserializeObject<List<AlgorithmTaskBasicResponseDto>>
+				(await result.Content.ReadAsStringAsync());
+
+			content!.Count
+				.Should()
+				.Be(2);
+
+			content[0].TaskTitle
+				.Should()
+				.Be("QuickSorting");
+
+			content[1].TaskTitle
+				.Should()
+				.Be("BubbleSorting");
+		}
+
+		[Fact]
+		public async Task Controller_GetAlgorithm_Should_ReturnResponseWith404Status_WhenAlgorithmNotFound()
+		{
+			DbSeeder(db => db.Add(new AlgorithmTask(Guid.NewGuid(), "BubbleSorting", "Test", "Test", AdvanceNumber.Create(5)!)));
+
+			var request = await _httpClient.GetAsync($"api/algorithms/{Guid.NewGuid()}");
+
+			request.StatusCode
+				.Should()
+				.Be(System.Net.HttpStatusCode.NotFound);
+		}
+
+		[Fact]
+		public async Task Controller_GetAlgorithm_Should_Return200ResponseWithProperlyMappedAlgorithm_WhenAlgorithmFound()
+		{
+			var id = Guid.NewGuid();
+			var title = "Sorting";
+			var taskMainTopics = "mainTopicSimple";
+
+			DbSeeder(db => db.Add(new AlgorithmTask(id, title, taskMainTopics, "", AdvanceNumber.Create(3)!)));
+
+			var result = await _httpClient.GetAsync($"api/algorithms/{id}");
+
+			var content = JsonConvert.DeserializeObject<AlgorithmTaskDetailResponseDto>
+				(await result.Content.ReadAsStringAsync());
+
+			result.StatusCode
+				.Should()
+				.Be(System.Net.HttpStatusCode.OK);
+
+			content!.Id
+				.Should()
+				.Be(id);
+
+			content!.TaskTitle
+				.Should()
+				.Be(title);
+
+			content!.TaskMainTopics
+				.Should()
+				.Be(taskMainTopics);
+		}
+	}
+}
